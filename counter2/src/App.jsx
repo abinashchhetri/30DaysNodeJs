a0a38ffea8ed26bb85d511c316655e859295d027
abinashchhetri <abinashchhetri.work@gmail.com>
2024-09-09 22:37:13 +0545

    counter app made

--- counter2/src/App.jsx
+++ counter2/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  let [counter, setCounter] = useState(1);
+
+  const addValue = (event) => {
+    counter = counter + 1;
+    counter >= 20
+      ? (event.target.disabled = true)
+      : (event.target.disabled = false);
+
+    setCounter(counter);
+  };
+
+  const decreaseValue = (event) => {
+    counter = counter - 1;
+    counter <= 0
+      ? (event.target.disabled = true)
+      : (event.target.disabled = false);
+    setCounter(counter);
+  };
+  return (
+    <>
+      <h1>Abinash Counter</h1>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={decreaseValue}>remover value</button>
+    </>
+  );
 }
 
 export default App;
